5c311b5b59c5b694660084cc8a6ec6476a5ee015
Dev-Madhu-3 <madhustudent4@gmail.com>
2026-02-20 00:21:06 +0530

    added server ERL

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://wimsecrm-backendv2-1.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
 
 api.interceptors.request.use(
